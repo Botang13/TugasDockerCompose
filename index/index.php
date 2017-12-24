3d633d0ca1136162585a68ad6cd78f3acc28608d
Botang13 <herbowoprasetyo@gmail.com>
2017-12-24 21:41:37 +0700

    file index.php

---
+++ index/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<b>";
+echo "<center><h1>Tugas Teori TCC 12 - Docker Compose</h1></center>";
+echo "<br>";
+echo "<h3>Nama 		: Herbowo Prasetyo<br>";
+echo "NIM 		: 155410070<br>";
+echo "Jurusan 		: Teknik Informatika<br>";
+echo "Github		: github.com/Botang13</h3>";
+echo "</b>";
+?>
